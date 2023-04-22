29c680e7876a0f5c2b1d439d938bfb7e85012f82
Chandrashekhar Rachakonda <shekhar.rachakonda@gmail.com>
2023-04-22 05:51:16 +0530

    break and continue

---
+++ continue.py
@@ -0,0 +1,18 @@
+# i=0
+# while i<=10:
+#     i+=1
+#     if i==5:
+#         continue
+#     print(i)
+
+
+# print("End of the program")
+
+
+for i in range(1,11):
+    print("before",i)
+    if i==5:
+        continue
+    print("after", i)
+
+print("End of the program")
